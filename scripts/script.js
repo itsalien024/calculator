0b473176169cc028666cdcd36c845dc97b19bfad
alien <148527832+itsalien024@users.noreply.github.com>
2023-12-11 11:16:27 +0300

    Final Design & Functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 let display = document.querySelector('#display');
+let currentContent = document.querySelector('#currentContent')
 let buttons = document.querySelectorAll('.buttons');
 let operatorButtons = document.querySelectorAll('.operatorButtons')
 
@@ -20,79 +21,130 @@ buttons.forEach(function(button) {
   button.addEventListener('click', function() {
     let buttonText = button.querySelector('.numbers').textContent;
     text = buttonText;
-    display.append(text);
+    display.textContent += text;
   })
 })
 
 multiply.addEventListener('click', function() {
-  currentSign = "*";
-  return currentSign;
+  if (currentValue.length === 0) {
+    currentValue.push(Number(display.textContent));
+    currentValue.push("*")
+    display.textContent = '';
+  }else if (currentValue.length === 2) {
+    currentValue.push(Number(display.textContent));
+    calculate();
+    currentValue = [result];
+    currentValue.push('*');
+    currentContent.textContent = '= ' + currentValue.join(' ');
+    display.textContent = '';
+  }else if (currentValue.length === 1) {
+    currentValue.push('*');
+    display.textContent = '';
+  }
 })
 
 divide.addEventListener('click', function() {
-  currentSign = "/";
-  return currentSign;
+  if (currentValue.length === 0) {
+    currentValue.push(Number(display.textContent));
+    currentValue.push("/")
+    display.textContent = '';
+  }else if (currentValue.length === 2) {
+    currentValue.push(Number(display.textContent));
+    calculate();
+    currentValue = [result];
+    currentValue.push('/');
+    currentContent.textContent = '= ' + currentValue.join(' ');
+    display.textContent = '';
+  }else if (currentValue === 1) {
+    currentValue.push('/');
+  }else if (currentValue.length === 1) {
+    currentValue.push('/');
+    display.textContent = '';
+  }
 })
 
 add.addEventListener('click', function() {
-  currentSign = "+";
-  return currentSign;
+  if (currentValue.length === 0) {
+    currentValue.push(Number(display.textContent));
+    currentValue.push("+")
+    display.textContent = '';
+  }else if (currentValue.length === 2) {
+    currentValue.push(Number(display.textContent));
+    calculate();
+    currentValue = [result];
+    currentValue.push('+');
+    currentContent.textContent = '= ' + currentValue.join(' ');
+    display.textContent = '';
+  }else if (currentValue.length === 1) {
+    currentValue.push('+');
+    display.textContent = '';
+  }
 })
 
 subtract.addEventListener('click', function() {
-  currentSign = "-";
-  return currentSign;
+  if (currentValue.length === 0) {
+    currentValue.push(Number(display.textContent));
+    currentValue.push("-")
+    display.textContent = '';
+  }else if (currentValue.length === 2) {
+    currentValue.push(Number(display.textContent));
+    calculate();
+    currentValue = [result];
+    currentValue.push('-');
+    currentContent.textContent = '= ' + currentValue.join(' ');
+    display.textContent = '';
+  }else if (currentValue.length === 1) {
+    currentValue.push('-');
+    display.textContent = '';
+  }
 })
 
 equals.addEventListener('click', function() {
-  currentSign = "=";
-  return currentSign;
+  if (currentValue.length === 2) {
+    currentValue.push(Number(display.textContent));
+    calculate();
+    display.textContent = 'Answer: ' + currentValue[0];
+    currentContent.textContent = '= ' + currentValue.join(' ');
+  }else if (currentValue.length === 3) {
+    calculate();
+  }
 })
 
 clear.addEventListener('click', function() {
-  currentSign = "c";
-  return currentSign;
+  currentValue = [];
+  display.textContent = '';
+  result = null;
+  currentContent.textContent = '';
 })
 
-function getValueAfter(currentSign) {
-  switch(currentSign) {
-    case "*":
-      result = currentValue[0] * currentValue[1];
-      currentValue[0] = result;
-      return currentValue
-    break;
-
-    case "/":
-      result = currentValue[0] / currentValue[1];
-      currentValue[0] = result;
-      return currentValue
-    break;
-
-    case "+":
-      result = currentValue[0] + currentValue[1];
-      currentValue[0] = result;
-      return currentValue
-    break;
-
-    case "-":
-      result = currentValue[0] - currentValue[1];
-      currentValue[0] = result;
-      return currentValue
-    break;
+function calculate(currentSign) {
+  if (currentValue.length === 3) {
+    switch(currentValue[1]) {
+      case "*":
+        result = currentValue[0] * currentValue[2];
+      break;
+  
+      case "/":
+        if (currentValue[2] == 0) {
+          alert('Stop It, you cant divide by 0');
+          currentValue = [];
+          display.textContent = 'Unable to divide by 0';
+          currentContent.textContent = '';
+        }else {
+          result = currentValue[0] / currentValue[2];
+        }
+      break;
+  
+      case "+":
+        result = currentValue[0] + currentValue[2];
+      break;
+  
+      case "-":
+        result = currentValue[0] - currentValue[2];
+      break;
+    }
+    currentValue = [result];
+    display.textContent = result; 
   }
 }
-
-
-operatorButtons.forEach(function operatorClear(opButton) {
-  opButton.addEventListener('click', function() {
-    if (currentValue.length < 2) {
-      currentValue.push((Number(display.textContent)));
-    }else if (currentValue.length = 2) {
-      getValueAfter();
-      display.textContent = String(result);
-    }
-    display.textContent = '';
-  })
-})
-
 
